406bf72637d708d57f88dbd94e0760569a10a849
Babulesny <misha271000@gmail.com>
2023-08-09 14:42:23 +0300

    merger updated, B1_2 created

--- B1_1/FileHandler.cs
+++ B1_1/FileHandler.cs
@@ -1,17 +1,16 @@
 ﻿using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
-using System.Data;
 using System.Text;
 
 namespace B1_1
 {
     internal class FileHandler
     {
-        private static int filesCount = 100;
-        private static int linesCount = 100000;
+        private static readonly int filesCount = 100;
+        private static readonly int linesCount = 100000;
         public static int Count = 0;
         private static readonly string selectNumbersSum = "SELECT SUM(CAST(num1 AS decimal)) FROM [B1_1].[dbo].[Lines]";
         private static readonly string selectNumbersMed = "SELECT TOP(1) Percentile_Disc(0.5) WITHIN GROUP(ORDER BY num2) OVER() FROM[B1_1].[dbo].[Lines]";
+        private static Mutex mutex = new();
         internal static void Generate()
         {
             Task[] tasks = new Task[filesCount];
@@ -27,9 +26,9 @@ namespace B1_1
         }
         private static Task GenerateFile(int c)
         {
-            Task task = new Task(() =>
+            Task task = new(() =>
             {
-                FileStream fs = new FileStream(string.Format("file{0}.txt", c.ToString()), FileMode.Create);
+                FileStream fs = new(string.Format("file{0}.txt", c.ToString()), FileMode.Create);
                 for (int i = 0; i < linesCount; i++)
                 {
                     var text = new DataModel();
@@ -39,19 +38,16 @@ namespace B1_1
             });           
             return task;
         }
-        internal static void MergeAsync(string str)
+        internal static void Merge(string str)
         {
-            FileStream fs = new FileStream("file.txt", FileMode.Create);
+            FileStream fs = new("file.txt", FileMode.Create);
 
-            if (str == null)
-            {
-                str = string.Empty;
-            }
+            str ??= string.Empty;
             Count = 0;
             Task[] tasks = new Task[filesCount];
             for (int i = 0; i < filesCount; i++)
             {
-                tasks[i] = MergeFileTask(i, str, fs);                
+                tasks[i] = MergeFile(i, str, fs);                
             }   
             
             foreach(Task task in tasks)
@@ -62,43 +58,20 @@ namespace B1_1
             fs.Close();
             Console.WriteLine("merged");
         }
-
-        private static Task MergeFileTask(int c, string str, FileStream fs)
+        private static Task MergeFile(int c, string str, FileStream fs)
         {            
-            Task task = new Task( () =>
+            Task task = new(() =>
             {
                 string filePath = string.Format("file{0}.txt", c.ToString());
                 List<string> line = File.ReadAllLines(filePath).ToList();
                 line.RemoveAll(s => s.IndexOf(str) > 0);
-                Count += linesCount - line.Count();
-                Task.Delay(10);
-                fs.Write(Encoding.Default.GetBytes(string.Join("\n", line) + "\n"));                
+                Count += linesCount - line.Count;
+                mutex.WaitOne();
+                fs.Write(Encoding.Default.GetBytes(string.Join("\n", line) + "\n"));   
+                mutex.ReleaseMutex();
             });
             return task;
-        }
-
-        internal static void Merge(string str)
-        {
-            FileStream fs = new FileStream("file.txt", FileMode.Create);
-
-            if (str == null)
-            {
-                str = string.Empty;
-            }
-            Count = 0;
-            for (int i = 0; i < filesCount; i++)
-            {
-                string filePath = string.Format("file{0}.txt", i.ToString());
-                List<string> line = File.ReadAllLines(filePath).ToList();
-                line.RemoveAll(s => s.IndexOf(str) > 0);
-                Count += linesCount - line.Count();
-                Task.Delay(10);
-                fs.Write(Encoding.Default.GetBytes(string.Join("\n", line) + "\n"));
-            }
-            fs.Close();
-            Console.WriteLine("merged");
-        }
-
+        }       
         internal static void Import(string path)
         {            
             using AppContext context = new();
@@ -119,16 +92,14 @@ namespace B1_1
                 Console.WriteLine($"!exeption! {e}");
             }            
         }
-
         internal static string CallStored()
         {            
             return $"sum = {CalculateNumbersSum()} median = {CalculateDoubleNumbersMedian()}";
         }
-
         public static decimal CalculateNumbersSum()
         {
             decimal res = 0;
-            AppContext db = new AppContext();
+            AppContext db = new();
             using (var connection = new SqlConnection(db.ConnectionString))
             {
                 connection.Open();
@@ -138,11 +109,10 @@ namespace B1_1
             }
             return res;
         }
-
         public static double CalculateDoubleNumbersMedian()
         {
             double res = 0.0;
-            AppContext db = new AppContext();
+            AppContext db = new();
             using (var connection = new SqlConnection(db.ConnectionString))
             {
                 connection.Open();
